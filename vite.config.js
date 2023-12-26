f714f6230893027618b8452c26db6bdc16e6cbda
unknown <atz.softprgmr@gmail.com>
2023-12-27 03:47:03 +0500

    added changes

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: 'https://phi.consulting/',
-  base: '/phi_consuting_react_website/',
-})
