b68d49c6c3de5a4ee4d1437f04b5585b7c03fda2
unknown <atz.softprgmr@gmail.com>
2023-12-27 20:01:02 +0500

    added changes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
-// vite.config.js
-export default {
-    base: '/phi_consulting_react_website/',
-  };
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/phi_consulting_react_website',
+  build: {
+    assetsDir: 'assets',
+  },
+});
