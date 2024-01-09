15c6a5ac5d52cc947553676ffb34a2ecfb944084
unknown <atz.softprgmr@gmail.com>
2024-01-10 00:05:06 +0500

    added changes

--- src/components/view_pages/CaseStudyView.jsx
+++ src/components/view_pages/CaseStudyView.jsx
@@ -29,15 +29,6 @@ const CaseStudyView = () => {
                     <p key={index} className="study-details">{item.text} </p> : <h3 key={index} className='study-heading'>{item.text}</h3>
                     
             ))}
-            {/* <p className="study-details">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum maxime, minima necessitatibus placeat voluptates perferendis et cum fuga, tempore dolore perspiciatis quas accusamus dicta eos numquam earum nesciunt veritatis ratione!
-                Dicta porro quidem nam aliquam in harum suscipit dolorum totam officiis eaque voluptatum accusantium iste magnam distinctio sed, debitis earum dolore tempora maiores natus? Nihil laboriosam accusantium veritatis dolore. Consequatur!
-                Quia ipsum eaque, tenetur quibusdam voluptas error explicabo magnam, nobis magni alias dolore mollitia esse. Illum accusantium quo iste beatae optio a amet similique dolorem! Eos nobis quod eligendi temporibus.
-                Et inventore nesciunt soluta molestiae aut hic sit voluptas, explicabo similique deserunt ipsam est? Reprehenderit perferendis commodi iure iusto aspernatur nisi blanditiis perspiciatis dolor incidunt harum eum, veritatis nam ab!
-                Inventore quibusdam perferendis autem nemo quos accusantium laborum repellat adipisci, voluptates reiciendis quae natus odio perspiciatis explicabo nam aspernatur non officia eum. Quam odit voluptatum, tempora expedita exercitationem tenetur iste?
-                Quaerat sint odio harum illum quis dignissimos inventore tempore nam pariatur possimus. Ex, odio? Atque incidunt, voluptate voluptatum, non facere aut odit fugiat minima, sed aperiam aliquid vitae pariatur rem.
-                Tempora facilis quibusdam impedit. Perspiciatis vitae beatae omnis debitis consequuntur. Rem, aliquam perferendis consequatur fugit nisi blanditiis voluptatibus, 
-            </p> */}
             <div class="case-more-button-container">
                 <div class="case-button-study-page-back">
                 <Link to="/" className='read-study-button-back'
