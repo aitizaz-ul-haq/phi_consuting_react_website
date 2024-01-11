9c6a0844cc89d9ab2f21fab3c87ceec70e3075da
unknown <atz.softprgmr@gmail.com>
2024-01-11 20:12:51 +0500

    added changes

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -15,33 +15,39 @@ const Header = () => {
       </div>
       <div className="buttons">
         <div className="dropdown">
-        <Link to="/services" className="dropdown-btn"><span className="dropdown-btn">Services</span></Link>
+        <Link to="/services" className="dropdown-btn"><span className="dropdown-btn">Solutions</span></Link>
           <div className="dropdown-content">
-            <Link to="sales-consulting">Sales Consulting</Link>
-            <Link to="buisness-consulting">Buisness Consulting</Link>
+            {/* <Link to="sales-consulting">Sales Consulting</Link> */}
+            <Link to="sales-consulting">GTM advisory</Link>
+
+            {/* <Link to="buisness-consulting">Buisness Consulting</Link> */}
             <Link to="financial-consulting">Financial Consulting</Link>
             <Link to="hr-consulting">HR & Recruitment Consulting</Link>
-            <Link to="customer-experience">Customer Experience Consulting</Link>
+            {/* <Link to="customer-experience">Customer Experience Consulting</Link> */}
           </div>
         </div>
+        <div className="dropdown">
+        <Link to="/services" className="dropdown-btn"><span className="dropdown-btn">Insights</span></Link>
+          <div className="dropdown-content">
+            <Link to="/insights">Blogs</Link>
+
+            <Link to="/spotlight">CaseStudies</Link>
+          </div>
+        </div>
+        <span
+          >
+            <Link to="/" className="inner-header">Industry</Link>
+          
+          </span
+        >
         <span>
             <Link to="/valuecreation" className="inner-header">Value Creation</Link>
             </span>
         <span
           > <Link to="/aboutus" className="inner-header">About us</Link></span
         >
-        <span
-          >
-            <Link to="/insights" className="inner-header">Insights</Link>
-          
-          </span
-        >
-        <span
-          >
-            <Link to="/spotlight" className="inner-header">Spotlight</Link>
-          
-          </span
-        >
+        
+        
         <span
           >
            <Link to="/careers" className="inner-header">Careers</Link>
