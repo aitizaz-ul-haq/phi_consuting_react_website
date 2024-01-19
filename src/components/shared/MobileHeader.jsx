9f8b8cb1219f563a362b6e63e06527774772c189
unknown <atz.softprgmr@gmail.com>
2024-01-19 20:12:29 +0500

    added changes

--- src/components/shared/MobileHeader.jsx
+++ src/components/shared/MobileHeader.jsx
@@ -43,63 +43,62 @@ const MobileHeader = () => {
             <div className="overlay-rock" onClick={closeMobileMenu}></div>
             <nav className="mobile-menu" id="mobile-menu">
               <ul>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/" className="header-mobile-link" onClick={closeMobileMenu}>
                  <img src={homeicon} className='mobile-icons' alt="" width={30} height={30}/> Home
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/services" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={solutionsicon} className='mobile-icons' alt="" width={32} height={32}/> Solutions
                 </a>
               </li>
-              
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/GTM-advisory" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={gtnicon} className='mobile-icons' alt="" width={32} height={32}/> GTM Strategy
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/financial-consulting" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={finicon} className='mobile-icons' alt="" width={32} height={32}/> Financial Consulting
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/investor-relations" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={invicon} className='mobile-icons' alt="" width={32} height={32}/> Investor Relations
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/hr-consulting" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={solutionsicon} className='mobile-icons' alt="" width={32} height={32}/>  hr & Recruitment 
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/valuecreation" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={valuecreation} className='mobile-icons' alt="" width={32} height={32}/> Value Creation
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/aboutus" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={aboutusicon} className='mobile-icons' alt="" width={30} height={30}/> About Us
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/blogs" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={blogicon} className='mobile-icons' alt="" width={32} height={32}/> Blogs
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/casestudies" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={casestudy} className='mobile-icons' alt="" width={32} height={32}/> Case Studies
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/careers" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={careersicon} className='mobile-icons' alt="" width={32} height={32}/> Careers
                 </a>
               </li>
-              <li>
+              <li className='list-element-mobile-menu'>
                 <a href="/contact" className="header-mobile-link" onClick={closeMobileMenu}>
                 <img src={contactsicon} className='mobile-icons' alt="" width={32} height={32}/>  Contact
                 </a>
