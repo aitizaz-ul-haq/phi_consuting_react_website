420a3bd0f8d479dc6daeeafbc315c1cef9947165
unknown <atz.softprgmr@gmail.com>
2024-01-23 03:24:32 +0500

    added changes

---
+++ src/components/shared/macroComps/IndustryServicesSection.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useRef } from 'react';
+
+import gtmicon from "../../../assets/img/solutions icons/GTM.png";
+import fin from "../../../assets/img/solutions icons/fin.png";
+import invrelations from "../../../assets//img/solutions icons/Investor Relations.png"
+import hricon from "../../../assets/img/white_icons/human-resources.png";
+const IndustryServicesSection = () => {
+
+    const ref = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('visible');
+                    } else {
+                        entry.target.classList.remove('visible');
+                    }
+                });
+            },
+            {
+                threshold: 0.1 // Adjust this value based on when you want the fade effect to trigger
+            }
+        );
+
+        const elements = ref.current.children;
+        for (let el of elements) {
+            observer.observe(el);
+        }
+
+        return () => {
+            for (let el of elements) {
+                observer.unobserve(el);
+            }
+        };
+    }, []);
+    return (
+        <>
+       <article className="services-industry-container">
+        <h2 className='services-industry-heading'>Our Services</h2>
+        <div className="service-cariers" ref={ref}>
+            <div className="service-nugget">
+                <div className="icon-carry">
+                    <img src={gtmicon} alt="" className="icon-services-industry" />
+                </div>
+                <div className="services-industry-title">
+                    GTM Advisory
+                </div>
+                <div className="services-industry-description">
+                Scale your sales operations with precision and excellence, targeting the right customers and optimizing sales cycles.
+                </div>
+            </div>
+
+            <div className="service-nugget">
+                <div className="icon-carry">
+                    <img src={fin} alt="" className="icon-services-industry" />
+                </div>
+                <div className="services-industry-title">
+                    Financial Consulting
+                </div>
+                <div className="services-industry-description">
+                Drive profitability and fiscal responsibility through strategic financial guidance.
+                </div>
+            </div>
+
+            <div className="service-nugget">
+                <div className="icon-carry">
+                    <img src={invrelations} alt="" className="icon-services-industry" />
+                </div>
+                <div className="services-industry-title">
+                    Investor Relations
+                </div>
+                <div className="services-industry-description">
+                Navigate challenges, optimize strategies, and achieve sustainable growth with our proven expertise.
+                </div>
+            </div>
+
+            <div className="service-nugget">
+                <div className="icon-carry">
+                    <img src={hricon} alt="" className="icon-services-industry" />
+                </div>
+                <div className="services-industry-title">
+                Hr & Recruitment
+                </div>
+                <div className="services-industry-description">
+                Build a dynamic and skilled workforce with our specialized HR solutions.
+                </div>
+            </div>
+        </div>
+       </article>
+        </>
+    )
+}
+
+export default IndustryServicesSection;
---
+++ src/components/shared/macroComps/IndustrySpecialities.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef } from 'react';
+
+const IndustrySpecialities = () => {
+    const sectionsRef = useRef([]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('visible');
+                    } else {
+                        entry.target.classList.remove('visible');
+                    }
+                });
+            },
+            {
+                rootMargin: '0px',
+                threshold: 0.6
+            }
+        );
+
+        const elements = sectionsRef.current;
+        elements.forEach(el => observer.observe(el));
+
+        return () => elements.forEach(el => observer.unobserve(el));
+    }, []);
+
+    const addToRefs = el => {
+        if (el && !sectionsRef.current.includes(el)) {
+            sectionsRef.current.push(el);
+        }
+    };
+
+    return(
+        <>
+       <article className="industry-para-container">
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'>What is <span className='bluer'>IOT Consulting</span>?</h2>
+        </div>
+        <div className="para-desc-industry">
+        IoT consulting services at Phi Consulting aim to empower companies by providing a deep understanding of IoT technologies, exploring relevant use cases, and crafting robust implementation strategies. This approach results in reduced operational costs, improved work environments, streamlined business processes, and enhanced products and services.
+        </div>
+        </section>
+       
+
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'>Tailored<span className='bluer'> Solutions</span>  for Your Unique Journey</h2>
+        </div>
+        <div className="para-desc-industry">
+        Recognizing the distinctive challenges faced by startup leaders, our custom consulting services are designed to cater to specific needs. Phi Consulting ensures your startup scales efficiently while maintaining cost-effectiveness.
+        </div>
+        </section>
+
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'>Revolutionize Your <span className='bluer'>Go-To-Market</span> Strategy</h2>
+        </div>
+        <div className="para-desc-industry">
+        In the dynamic world of IoT and tech startups, a well-crafted go-to-market (GTM) strategy is not just advantageous but essential. Our seasoned experts develop custom GTM strategies aligned with your vision, enabling you to not only enter the market but also make a lasting impact.
+        </div>
+        </section>
+
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'><span className='bluer'>Attract, Retain,</span> and <span className='bluer'>Nurture</span> the Best Talent</h2>
+        </div>
+        <div className="para-desc-industry">
+        Your team is the backbone of success. Phi Consulting offers comprehensive HR and recruitment services, assisting you in building a talented workforce aligned with your startup's culture and objectives. From talent acquisition to development, we ensure your human capital is your strongest asset.
+        </div>
+        </section>
+
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'>Navigate <span className='bluer'>Financial Complexities</span> with Ease</h2>
+        </div>
+        <div className="para-desc-industry">
+        Managing finances in a startup environment can be challenging. Our IoT consulting services provide clarity and direction, helping you make informed decisions that secure your company's financial health and fuel its growth.
+        </div>
+        </section>
+
+        <section className="industry-para-collection" ref={addToRefs}>
+        <div className="para-title-industry">
+        <h2 className='title-special'><span className='bluer'>Building Bridges</span> Between You and Your Investors</h2>
+        </div>
+        <div className="para-desc-industry">
+        Strong investor relations are vital for startup growth. Our team helps you communicate your vision and progress effectively, fostering trust and understanding with investors. This ensures not only continued financial support but also strategic partnerships that propel your IoT startup forward.
+        </div>
+        </section>
+       </article>
+        </>
+    );
+}
+
+export default IndustrySpecialities;
