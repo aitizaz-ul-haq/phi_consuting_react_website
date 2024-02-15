ecced063d7610dfae1d2c8bdbadd33baf8273a45
unknown <atz.softprgmr@gmail.com>
2024-02-15 19:56:39 +0500

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -260,7 +260,7 @@ const breadcrumbSchema =  {
         <Route path="/case-studies" element={<Layout><Spotlight /></Layout>} />
         <Route path="casestudy/:companyName" element={<Layout><CaseStudyView /></Layout>} />
         {/* <Route path="casestudy/:id" element={<Layout><CaseStudyView /></Layout>} /> */}
-        <Route path="/viewcasestudy/:caseId" element={<Layout><ViewCasestudy /></Layout>} />
+        {/* <Route path="/viewcasestudy/:caseId" element={<Layout><ViewCasestudy /></Layout>} /> */}
         <Route path="casedetails/:id" element={<Layout><CaseDetails /></Layout>} />
         <Route path="blog/:id" element={<Layout><BlogView/></Layout>}/>
 
