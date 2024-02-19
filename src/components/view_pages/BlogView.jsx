3d1582e39c31311b267344d5479d53f84849e48d
unknown <atz.softprgmr@gmail.com>
2024-02-20 03:23:51 +0500

    added changes

--- src/components/view_pages/BlogView.jsx
+++ src/components/view_pages/BlogView.jsx
@@ -98,43 +98,8 @@ const BlogView = () => {
     </button>
     </Tooltip>
             </div>
-          {/* <article className="blog-viewer">
-            <section className="blog-content-container">
-                <div className="blog-content">
-                    <div className="blog-image-header-container">
-                        <img src={blogHeader} alt="" className='blog-image-in-reader' />
-                    </div>
-                   <div className="blog-heading-read-section">
-                   {blog.title}
-                   </div>
-                   <div className="blog-desc-line-section">
-                    {blog.summary}
-                   </div>
-                   <hr />
-                   <div className="blog-content-text-section">
-                   {blog.content.map((item, index) => (
-                item.type === 'paragraph' ? 
-                    <p key={index} className="blog-para-spacing">{item.text} </p> : <h3 key={index} className='study-heading'>{item.text}</h3>
-                    
-            ))}
-                   </div>
-                   <div class="blog-more-button-container">
-                <div class="blog-button-study-page-back">
-               
-                <Link to="/" className='blog-study-button-back'
-                  > &larr; Back</Link
-                >
-              
-                     </div>
-                <div class="blog-button-study-page"> <Link to="/blogs" className='blog-study-button-back'
-                  > View More Blogs &rarr;</Link
-                > </div>
-              </div>
-                </div>
-            </section>
-          </article> */}
 
-<article className="blog-viewer">
+             <article className="blog-viewer">
                 <section className="blog-content-container">
                     <div className="blog-content">
                         <img src={blog.imageUrl} alt="Blog header" className='blog-image-in-reader' />
