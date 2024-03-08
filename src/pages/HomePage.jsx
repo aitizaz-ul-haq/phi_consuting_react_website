b00d9dadbfab0b7af8a812e002776ea3f88e20f5
unknown <atz.softprgmr@gmail.com>
2024-03-08 19:24:21 +0500

    added changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -433,11 +433,11 @@ const HomePage = () => {
   return (
     <>
      <Helmet>
-  <title>Phi Consulting - Home</title>
-  <meta name="description" content="Phi Consulting is your trusted tech consulting partner, specializing in startups & SMEs in IoT, Fintech, SaaS, & IaaS. Expertise in Go-To-Market Strategy, HR, Investor Relations & Financial Consulting. Schedule a free consultation." />
+       <title>Phi Consulting - Home</title>
+       <meta name="description" content="Phi Consulting is your trusted tech consulting partner, specializing in startups & SMEs in IoT, Fintech, SaaS, & IaaS. Expertise in Go-To-Market Strategy, HR, Investor Relations & Financial Consulting. Schedule a free consultation." />
     </Helmet>
      <Helmet>
-      <link rel="canonical" href="https://phiconsulting.org/home" />
+       <link rel="canonical" href="https://phiconsulting.org/home" />
     </Helmet>
 
     <div className={`overlayscreen ${darkMode ? 'activate' : ''}`}></div>
